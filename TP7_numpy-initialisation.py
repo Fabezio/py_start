2d7b3efe4c28bf026a61a8f539f298e0f88ebfd1
fabezio <fabezio@outlook.fr>
2021-01-27 11:15:05 +0100

    TP: numpy

--- TP7_numpy-initialisation.py
+++ TP7_numpy-initialisation.py
@@ -1,4 +1,14 @@
 import numpy as np
+from numpy import int8
 
-a = np.zeros((3,2))
+b = np.ones((3,2), dtype=int8)
+a = np.zeros((3,2), dtype=int8)
+a = a.ravel()
+b = b.ravel()
 print(a)
+print(b)
+#print(type(a.size))
+ab = np.concatenate((a,b), axis=0)
+abSize = ab.size
+#ab = ab.reshape((6, int(abSize/2)))
+print(abSize)
