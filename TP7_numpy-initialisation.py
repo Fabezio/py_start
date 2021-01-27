45bfadf4996db1b7bf43842f927341a592fe932d
fabezio <fabezio@outlook.fr>
2021-01-27 13:35:27 +0100

    TP: Numpy (corrigé)

--- TP7_numpy-initialisation.py
+++ TP7_numpy-initialisation.py
@@ -1,14 +1,15 @@
 import numpy as np
 from numpy import int8
 
-b = np.ones((3,2), dtype=int8)
-a = np.zeros((3,2), dtype=int8)
-a = a.ravel()
-b = b.ravel()
-print(a)
-print(b)
-#print(type(a.size))
-ab = np.concatenate((a,b), axis=0)
-abSize = ab.size
-#ab = ab.reshape((6, int(abSize/2)))
-print(abSize)
+r = int(input("nombre de rangées: "))
+c = int(input("nombre de colonnes: "))
+def initialisation(m, n):
+    # np.random.seed(0)
+    tab = np.random.randn(m, n)
+    # col = np.ones((m, 1), dtype=int8)
+    # print(col)
+    tab = np.concatenate((tab, np.ones((tab.shape[0], 1), dtype=int8)), axis=1)
+    return tab
+
+
+print(initialisation(r, c))
